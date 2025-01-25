391f6c3f8eb974a0d23b8907a3a4fc207f24d05f
Saray Rodríguez <rodriguezsaray602@gmail.com>
2025-01-25 07:03:48 +0000

    falta diseño

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,49 +1,47 @@
 import React, { useState } from "react";
 
-
-
-//create your first component
+// Create your first component
 const Home = () => {
-    const [inputValue, setInputValue ] = useState("");
-    const [todos, setTodos] = useState ([]);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-    return (
-        <div className="container">
-           <h1> To Do List </h1>
-           <ul>
-                <li>
-                    <input type="text"
-					onChange={(e) => setInputValue(e.target.value)}
-					value={inputValue} 
-                    onKeyPress={(e) =>{
-                        if (e.key === "Enter") {
-                            setTodos(todos.concat (inputValue));
-                            setInputValue("");
-                        }
-                    }} 
-                    placeholder="your task"></input>
-
-                </li>
-                {todos.map((item, index) => (
-                     <li> 
-                        {item}{" "}
-                        
-						class=<i>"fa-solid fa-delete-left"</i>
-                            onClick={()=> 
-                            setTodos(
-                                todos.filter(
-                                    (t, currentIndex)=>
-                                    	 index != currentIndex
-                                )
-                            )
-                        }
-                </li>
-            ))}
-        </ul>
-
-            
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>To Do List</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="your task"
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item} {" "}
+            <i
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index !== currentIndex)
+                )
+              }
+            ></i>
+            <button onClick={() => setTodos(todos.filter((_, i) => i !== index))}>
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+              <div>{todos.length} task </div>
+    </div>
+  );
 };
 
 export default Home;
